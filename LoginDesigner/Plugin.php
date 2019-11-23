a3e820e257449e9a6065d884a0c24a6fa8caacf6
jrotty <bssf@qq.com>
2019-11-23 14:10:20 +0800

    升级至1.2.0
    1，修复自定义背景图样式问题
    2，新增一款高斯模糊主题
    3，新增一款纯黑主题
    4，新增一款绿色动态主题
    5，插件接口变为顶部+底部

--- LoginDesigner/Plugin.php
+++ LoginDesigner/Plugin.php
@@ -5,8 +5,8 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package Login Designer
  * @author 泽泽社长
- * @version 1.1.1
- * @link http://qqdie.com
+ * @version 1.2.0
+ * @link https://qqdie.com/archives/typecho-LoginDesigner-plugin.html
  */
 class LoginDesigner_Plugin implements Typecho_Plugin_Interface
 {
@@ -20,6 +20,7 @@ class LoginDesigner_Plugin implements Typecho_Plugin_Interface
     public static function activate()
     {
         Typecho_Plugin::factory('admin/header.php')->header = array('LoginDesigner_Plugin', 'render');
+        Typecho_Plugin::factory('admin/footer.php')->end = array('LoginDesigner_Plugin', 'footerjs');
     }
     
     /**
@@ -43,22 +44,28 @@ class LoginDesigner_Plugin implements Typecho_Plugin_Interface
     {
  
 ?>
-<link rel="stylesheet" href="<?php Helper::options()->pluginUrl(); ?>/LoginDesigner/style.css?20191122">
+<link rel="stylesheet" href="<?php Helper::options()->pluginUrl(); ?>/LoginDesigner/style.css?20191123">
 <?php
 $url=Helper::options()->pluginUrl.'/LoginDesigner/';
 $zz1='<div class="zz">素雅<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
 $zz2='<div class="zz">黑色<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
 $zz3='<div class="zz">白色<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
-$zz4='<div class="zz">黑客帝国<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
-$zz5='<div class="zz">纯白<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';    
+$zz4='<div class="zz">纯白<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
+$zz5='<div class="zz">纯黑<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
+$zz6='<div class="zz">黑客帝国<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
+$zz7='<div class="zz">高斯模糊<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>'; 
+$zz8='<div class="zz">方绿<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';  
       
  $bgfengge = new Typecho_Widget_Helper_Form_Element_Radio(
 'bgfengge', array(
   'suya' => _t('<div class="kuai"><img src="'.$url.'/images/suya.jpg" loading="lazy">'.$zz1.'</div>'),
   'black' => _t('<div class="kuai"><img src="'.$url.'/images/black.jpg" loading="lazy" loading="lazy">'.$zz2.'</div>'),
   'white' => _t('<div class="kuai"><img src="'.$url.'/images/white.jpg" loading="lazy">'.$zz3.'</div>'),
-  'heike' => _t('<div class="kuai tags"><img src="'.$url.'/images/heike.jpg" loading="lazy">'.$zz4.'</div>'),
-  'bai' => _t('<div class="kuai"><img src="'.$url.'/images/bai.jpg" loading="lazy">'.$zz5.'</div>'),
+  'bai' => _t('<div class="kuai"><img src="'.$url.'/images/bai.jpg" loading="lazy">'.$zz4.'</div>'),
+  'hei' => _t('<div class="kuai"><img src="'.$url.'/images/hei.jpg" loading="lazy">'.$zz5.'</div>'),
+  'heike' => _t('<div class="kuai tags"><img src="'.$url.'/images/heike.jpg" loading="lazy">'.$zz6.'</div>'),
+  'mohu' => _t('<div class="kuai"><img src="'.$url.'/images/mohu.jpg" loading="lazy">'.$zz7.'</div>'),
+  'lv' => _t('<div class="kuai tags"><img src="'.$url.'/images/lv.jpg" loading="lazy">'.$zz8.'</div>'),
     ), 'suya', _t('登陆/注册页面样式'), _t('')); $bgfengge->setAttribute('id', 'yangshi');
     $form->addInput($bgfengge); 
       
@@ -91,20 +98,39 @@ $skin=Typecho_Widget::widget('Widget_Options')->plugin('LoginDesigner')->bgfengg
   
 
 if($skin=='heike'){
-$hed=$hed. '<link rel="stylesheet" href="'.$url.'skin/'.$skin.'.css"><canvas id="canvas"></canvas><script type="text/javascript">window.onload=function(){var canvas=document.getElementById("canvas");var context=canvas.getContext("2d");var W=window.innerWidth;var H=window.innerHeight;canvas.width=W;canvas.height=H;var fontSize=16;var colunms=Math.floor(W/fontSize);var drops=[];for(var i=0;i<colunms;i++){drops.push(0)}var str="001";function draw(){context.fillStyle="rgba(0,0,0,0.05)";context.fillRect(0,0,W,H);context.font="700 "+fontSize+"px  微软雅黑";context.fillStyle="#00cc33";for(var i=0;i<colunms;i++){var index=Math.floor(Math.random()*str.length);var x=i*fontSize;var y=drops[i]*fontSize;context.fillText(str[index],x,y);if(y>=canvas.height&&Math.random()>0.99){drops[i]=0}drops[i]++}}function randColor(){var r=Math.floor(Math.random()*256);var g=Math.floor(Math.random()*256);var b=Math.floor(Math.random()*256);return"rgb("+r+","+g+","+b+")"}draw();setInterval(draw,30)};</script>';
-
+$hed=$hed. '<link rel="stylesheet" href="'.$url.'skin/'.$skin.'.css?20191123">';
 
 
 }else{
 $bgUrl=Typecho_Widget::widget('Widget_Options')->plugin('LoginDesigner')->bgUrl;
 $zidingyi="";
 if($bgUrl){
-  $zidingyi="<style>body{background: url(".$bgUrl.")}</style>";
+  $zidingyi="<style>body,body::before{background-image: url(".$bgUrl.")}</style>";
 }
-$hed=$hed. '<link rel="stylesheet" href="'.$url.'skin/'.$skin.'.css">'.$zidingyi;
+$hed=$hed. '<link rel="stylesheet" href="'.$url.'skin/'.$skin.'.css?20191123">'.$zidingyi;
 }
 
 }
         return $hed;
     }
+  
+    public static function footerjs()
+    {  
+  if (!Typecho_Widget::widget('Widget_User')->hasLogin()){
+$url=Helper::options()->pluginUrl.'/LoginDesigner/';
+$skin=Typecho_Widget::widget('Widget_Options')->plugin('LoginDesigner')->bgfengge;
+$ft='';
+if($skin=='heike'){
+$ft='<canvas id="canvas"></canvas><script type="text/javascript">window.onload=function(){var canvas=document.getElementById("canvas");var context=canvas.getContext("2d");var W=window.innerWidth;var H=window.innerHeight;canvas.width=W;canvas.height=H;var fontSize=16;var colunms=Math.floor(W/fontSize);var drops=[];for(var i=0;i<colunms;i++){drops.push(0)}var str="001";function draw(){context.fillStyle="rgba(0,0,0,0.05)";context.fillRect(0,0,W,H);context.font="700 "+fontSize+"px  微软雅黑";context.fillStyle="#00cc33";for(var i=0;i<colunms;i++){var index=Math.floor(Math.random()*str.length);var x=i*fontSize;var y=drops[i]*fontSize;context.fillText(str[index],x,y);if(y>=canvas.height&&Math.random()>0.99){drops[i]=0}drops[i]++}}function randColor(){var r=Math.floor(Math.random()*256);var g=Math.floor(Math.random()*256);var b=Math.floor(Math.random()*256);return"rgb("+r+","+g+","+b+")"}draw();setInterval(draw,30)};</script>';
+}
+if($skin=='lv'){
+$ft='<ul class="bg-bubbles"><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul>';
+} 
+echo $ft;
+  }  }
+  
+  
+  
+  
+  
 }
