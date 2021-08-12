31f1019a9f3ebf92edb3c65c4a26f34d3283f42b
jrotty <bssf@qq.com>
2021-08-12 10:30:06 +0800

    更新至1.3.5
    新增哔哩哔哩登录样式，去除空白样式

--- LoginDesigner/Plugin.php
+++ LoginDesigner/Plugin.php
@@ -5,7 +5,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * 
  * @package Login Designer
  * @author 泽泽社长
- * @version 1.3.0
+ * @version 1.3.5
  * @link https://zezeshe.com/archives/typecho-LoginDesigner-plugin.html
  */
 class LoginDesigner_Plugin implements Typecho_Plugin_Interface
@@ -44,19 +44,19 @@ class LoginDesigner_Plugin implements Typecho_Plugin_Interface
     {
  
 ?>
-<link rel="stylesheet" href="<?php Helper::options()->pluginUrl(); ?>/LoginDesigner/style.css?20191123">
+<link rel="stylesheet" href="<?php Helper::options()->pluginUrl(); ?>/LoginDesigner/style.css?202108">
 <?php
 $url=Helper::options()->pluginUrl.'/LoginDesigner/';
-$zz1='<div class="zz">素雅山水<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
-$zz2='<div class="zz">蓝天群山<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
-$zz3='<div class="zz">早春印象<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
-$zz4='<div class="zz">海洋巨人<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>'; 
-$zz5='<div class="zz">绿意之方<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';  
-$zz6='<div class="zz">暗之色系<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>'; 
-$zz7='<div class="zz">亮之色系<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
-$zz8='<div class="zz">黑客帝国<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>';
-$zz9='<div class="zz">高斯模糊<br>作者：<a href="http://qqdie.com" target="_blank">泽泽社长</a></div>'; 
-$zz10='<div class="zz">空白样式<br>不使用内置样式</div>'; 
+$zz1='<div class="zz">素雅山水</div>';
+$zz2='<div class="zz">蓝天群山</div>';
+$zz3='<div class="zz">早春印象</div>';
+$zz4='<div class="zz">海洋巨人</div>'; 
+$zz5='<div class="zz">绿意之方</div>';  
+$zz6='<div class="zz">暗之色系</div>'; 
+$zz7='<div class="zz">亮之色系</div>';
+$zz8='<div class="zz">黑客帝国</div>';
+$zz9='<div class="zz">高斯模糊</div>'; 
+$zz10='<div class="zz">哔哩哔哩</div>'; 
       
  $bgfengge = new Typecho_Widget_Helper_Form_Element_Radio(
 'bgfengge', array(
@@ -69,7 +69,7 @@ $zz10='<div class="zz">空白样式<br>不使用内置样式</div>';
   'white' => _t('<div class="kuai"><img src="'.$url.'/images/white.jpg" loading="lazy">'.$zz7.'</div>'),
   'heike' => _t('<div class="kuai tags"><img src="'.$url.'/images/heike.jpg" loading="lazy">'.$zz8.'</div>'),
   'mohu' => _t('<div class="kuai"><img src="'.$url.'/images/mohu.jpg" loading="lazy">'.$zz9.'</div>'),
-  'kongbai' => _t('<div class="kuai"><img src="'.$url.'/images/kongbai.jpg" loading="lazy">'.$zz10.'</div>'),
+  'bilibili' => _t('<div class="kuai"><img src="'.$url.'/images/bilibili.jpg" loading="lazy">'.$zz10.'</div>'),
     ), 'suya', _t('登陆/注册页面样式'), _t('')); $bgfengge->setAttribute('id', 'yangshi');
     $form->addInput($bgfengge); 
       
@@ -108,14 +108,14 @@ if($skin=='kongbai'){
 $hed=$hed. '<style>'.$diycss.'</style>';
 }else{
 if($skin=='heike'){
-$hed=$hed. '<link rel="stylesheet" href="'.$url.'skin/'.$skin.'.css?20191125">';
+$hed=$hed. '<link rel="stylesheet" href="'.$url.'skin/'.$skin.'.css?2021">';
 }else{
 $bgUrl=Typecho_Widget::widget('Widget_Options')->plugin('LoginDesigner')->bgUrl;
 $zidingyi="";
 if($bgUrl){
   $zidingyi="<style>body,body::before{background-image: url(".$bgUrl.")}</style>";
 }
-$hed=$hed. '<link rel="stylesheet" href="'.$url.'skin/'.$skin.'.css?20191125">'.$zidingyi;
+$hed=$hed. '<link rel="stylesheet" href="'.$url.'skin/'.$skin.'.css?2021">'.$zidingyi;
 }
 }
 
@@ -132,9 +132,38 @@ $ft='';
 if($skin=='heike'){
 $ft='<canvas id="canvas"></canvas><script type="text/javascript">window.onload=function(){var canvas=document.getElementById("canvas");var context=canvas.getContext("2d");var W=window.innerWidth;var H=window.innerHeight;canvas.width=W;canvas.height=H;var fontSize=16;var colunms=Math.floor(W/fontSize);var drops=[];for(var i=0;i<colunms;i++){drops.push(0)}var str="111001101000100010010001111001111000100010110001111001001011110110100000";function draw(){context.fillStyle="rgba(0,0,0,0.05)";context.fillRect(0,0,W,H);context.font="700 "+fontSize+"px  微软雅黑";context.fillStyle="#00cc33";for(var i=0;i<colunms;i++){var index=Math.floor(Math.random()*str.length);var x=i*fontSize;var y=drops[i]*fontSize;context.fillText(str[index],x,y);if(y>=canvas.height&&Math.random()>0.99){drops[i]=0}drops[i]++}}function randColor(){var r=Math.floor(Math.random()*256);var g=Math.floor(Math.random()*256);var b=Math.floor(Math.random()*256);return"rgb("+r+","+g+","+b+")"}draw();setInterval(draw,30)};</script>';
 }
-if($skin=='lv'){
+elseif($skin=='lv'){
 $ft='<ul class="bg-bubbles"><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li></ul>';
 } 
+elseif($skin=='bilibili'){
+$ft='<script type="text/javascript">
+            $(function(){
+            $(\'.typecho-login h1\').replaceWith(\'<div id="l23"></div><div id="r23"></div>\');
+                var password = $(\'#password\');
+                var confirm = $(\'#confirm\');
+                var l23 = $(\'#l23\');
+                var r23 = $(\'#r23\');
+                confirm.bind(\'focus\', function(){
+                    password.removeClass(\'error\');
+                    l23.css(\'background-image\', \'url('.$url.'img/222.png)\');
+                    r23.css(\'background-image\', \'url('.$url.'img/333.png)\');
+                });
+                confirm.blur(function(){
+                    l23.css(\'background-image\', \'url('.$url.'img/22.png)\');
+                    r23.css(\'background-image\', \'url('.$url.'img/33.png)\');
+                });
+                password.bind(\'focus\', function(){
+                    password.removeClass(\'error\');
+                    l23.css(\'background-image\', \'url('.$url.'img/222.png)\');
+                    r23.css(\'background-image\', \'url('.$url.'img/333.png)\');
+                });
+                password.blur(function(){
+                    l23.css(\'background-image\', \'url('.$url.'img/22.png)\');
+                    r23.css(\'background-image\', \'url('.$url.'img/33.png)\');
+                });
+                });
+</script>\'); ';
+}
 echo $ft;
   }  }
   
